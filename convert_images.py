36f48094dc21dc7b22fb9a7731c8342914db01f1
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-23 22:36:40 -0400

    Import sys module and use os.listdir()
    Imported sys module and used os.listdir() to iterate through images in folder.

--- convert_images.py
+++ convert_images.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 from PIL import Image
-import os
+import os, sys
 
 original_directory = "/Users/nakoya/Documents/Python/google-it-automation-cert/images"
 new_directory = "/Users/nakoya/Documents/Python/google-it-automation-cert/opt/icons/"
-for image in original_directory:
+images = os.listdir(original_directory)
+
+for image in images:
     image_name, image_extension = os.path.splitext(image)
     converted_image = image_name + ".jpg"
     if image != converted_image:
@@ -13,3 +15,4 @@ for image in original_directory:
                 im.rotate(270).resize((128,128)).save(new_directory + converted_image)
         except OSError:
             print("Cannot convert image.", image)
+
