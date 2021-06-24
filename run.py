639513d80ea921b7308708bfa03c5e5f568d7a41
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-24 11:45:35 -0400

    Create list off all files and dictionary of contents
    Created a list of all .txt files in directory. Then traversed over
    each file and from the contents of the files, created a dictionary
    using title, name, date and feedback as keys for the content value,
    respectively.

---
+++ run.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+import os
+import requests
+
+directory_path = "/data/feedback/"
+feedback_files = os.listdir(directory_path)
+for file in feedback_files:
+    feedback_dictionary = {}
+    with open(os.path.join(directory_path, file)) as f:
+        lines = f.read().splitlines()
+        title, name, date, feedback = lines
+        feedback_dictionary["title"] = title
+        feedback_dictionary["name"] = name
+        feedback_dictionary["date"] = date
+        feedback_dictionary["feedback"] = feedback
