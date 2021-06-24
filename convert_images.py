08d4911b1d704b8e586d7eecea4d4bbe500996a9
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-24 09:27:27 -0400

    Change file paths

--- convert_images.py
+++ convert_images.py
@@ -2,16 +2,16 @@
 from PIL import Image
 import os
 
-original_directory = "/Users/nakoya/Documents/Python/google-it-automation-cert/images/"
-new_directory = "/Users/nakoya/Documents/Python/google-it-automation-cert/opt/icons/"
+original_directory = "/home/student-01-af640c4e9338/images/"
+new_directory = "/opt/icons/"
 images = os.listdir(original_directory)
 
 for image in images:
     image_name, image_extension = os.path.splitext(image)
-    converted_image = image_name + ".jpeg"
+    converted_image = image_name + ".jpg"
     if image != converted_image:
         try:
             with Image.open(os.path.join(original_directory, image)) as im:
-                im.rotate(270).resize((128,128)).convert("RGB").save(os.path.join(new_directory, converted_image))
+                im.rotate(270).resize((128,128)).convert("RGB").save(new_directory + converted_image)
         except OSError:
             print("Image conversion failed.", image)
