cc1e48d05f13d63064bb69b69723012ba50ba4ba
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-24 09:03:42 -0400

    Use os.path.join() and convert("RGB")

--- convert_images.py
+++ convert_images.py
@@ -8,10 +8,10 @@ images = os.listdir(original_directory)
 
 for image in images:
     image_name, image_extension = os.path.splitext(image)
-    converted_image = image_name + ".jpg"
+    converted_image = image_name + ".jpeg"
     if image != converted_image:
         try:
-            with Image.open(original_directory + image) as im:
-                im.rotate(270).resize((128,128)).save(new_directory + converted_image)
+            with Image.open(os.path.join(original_directory, image)) as im:
+                im.rotate(270).resize((128,128)).convert("RGB").save(os.path.join(new_directory, converted_image))
         except OSError:
             print("Image conversion failed.", image)
