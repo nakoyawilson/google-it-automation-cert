f5f93af74e7bc5e1c75203faa2f5ce2e52d04579
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-25 10:42:45 -0400

    Take images in directory and upload to web server

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+import requests
+
+directory = "~/supplier-data/images/"
+images = os.listdir(directory)
+url = "http://localhost/upload"
+with open(images) as opened:
+    for image in images:
+        response = requests.post(url, files={"file": opened})
