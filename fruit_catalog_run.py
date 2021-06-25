5325294a5c03c7b0f280de4dd1c43e4e6345fc7a
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-25 15:31:25 -0400

    Remove 'rb' mode and add .endswith() function
    Removed 'rb' mode and added .endswith(.txt) to ensure only .txt files are
    opened and processed.

--- fruit_catalog_run.py
+++ fruit_catalog_run.py
@@ -7,17 +7,18 @@ directory_path = "~/supplier-data/descriptions/"
 descriptions = os.listdir(directory_path)
 
 for text_file in descriptions:
-    file_name, file_extension = os.path.splitext(text_file)
-    image_name = file_name + ".jpeg"
-    description_dictionary = {}
-    with open(os.path.join(directory_path, text_file), 'rb') as d:
-        lines = d.read().splitlines()
-        name, weight, description = lines
-        weight_as_list = weight.split()
-        weight = int(weight_as_list[0])
-        description_dictionary["name"] = name
-        description_dictionary["weight"] = weight
-        description_dictionary["description"] = description
-        description_dictionary["image_name"] = image_name
+    if text_file.endswith(".txt"):
+        file_name, file_extension = os.path.splitext(text_file)
+        image_name = file_name + ".jpeg"
+        description_dictionary = {}
+        with open(os.path.join(directory_path, text_file)) as d:
+            lines = d.read().splitlines()
+            name, weight, description = lines
+            weight_as_list = weight.split()
+            weight = int(weight_as_list[0])
+            description_dictionary["name"] = name
+            description_dictionary["weight"] = weight
+            description_dictionary["description"] = description
+            description_dictionary["image_name"] = image_name
     response = requests.post("http://[linux-instance-external-IP]/fruits/", data=description_dictionary)
     response.raise_for_status()
