738fe2fcf5dac517a9b779249eb6b910526bcfd5
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-25 22:14:48 -0400

    Fix code that generates report

--- reports.py
+++ reports.py
@@ -2,10 +2,10 @@
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 
-def generate_report(filename, title, additional_info):
-    styles = getSampleStyleSheet()
+def generate_report(filename, title, new_report_data):
     report = SimpleDocTemplate(filename)
+    styles = getSampleStyleSheet()
     report_title = Paragraph(title, styles["h1"])
-    report_info = Paragraph(additional_info, styles["BodyText"])
+    report_info = Paragraph(new_report_data, styles["BodyText"])
     empty_line = Spacer(1,20)
     report.build([report_title, empty_line, report_info])
