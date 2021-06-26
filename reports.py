b31d4542b174af8540376b74d0958f84f316b6e9
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-06-25 22:21:01 -0400

    Change argument names

--- reports.py
+++ reports.py
@@ -2,10 +2,10 @@
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 
-def generate_report(filename, title, new_report_data):
-    report = SimpleDocTemplate(filename)
+def generate_report(attachment, title, paragraph):
+    report = SimpleDocTemplate(attachment)
     styles = getSampleStyleSheet()
     report_title = Paragraph(title, styles["h1"])
-    report_info = Paragraph(new_report_data, styles["BodyText"])
+    report_info = Paragraph(paragraph, styles["BodyText"])
     empty_line = Spacer(1,20)
     report.build([report_title, empty_line, report_info])
